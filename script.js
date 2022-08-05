ca083f074fe60896c50aa8ad5e2a5e68cd37a88c
YSWickramatantri <yowun2008@gmail.com>
2022-08-05 10:36:19 +0530

    Made grid

---
+++ script.js
@@ -0,0 +1,7 @@
+for (let i = 0; i < 36; i++) {
+    var gridBox = document.createElement("div");
+    gridBox.className = "grid-box";
+    gridBox.style.width = "100px";
+    gridBox.style.height = "100px";
+    document.getElementById("grid-container").appendChild(gridBox);    
+}
