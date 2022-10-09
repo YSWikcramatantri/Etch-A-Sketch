88f3ae2bfa1b55c23707d9c408e4293200f002b3
YSWickramatantri <yowun2008@gmail.com>
2022-10-09 16:48:29 +0530

    Finished

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ for (let i = 0; i < 36; i++) {
     var gridBox = document.createElement("div");
     gridBox.className = "grid-box";
     gridBox.id = "grid-box"+i;
-    gridBox.style.width = "100px";
-    gridBox.style.height = "100px";
+    gridBox.style.width = "80px";
+    gridBox.style.height = "80px";
     gridBox.addEventListener("mouseover", changeColor);
     document.getElementById("grid-container").appendChild(gridBox); 
 };
@@ -12,4 +12,20 @@ for (let i = 0; i < 36; i++) {
 function changeColor() {
     var box = document.getElementById(this.id);
     box.style.backgroundColor = "black";
+}
+
+function changeGrid() {
+    var size = prompt("Enter the size of grid you want:");
+    var boxSize = 500/(Math.sqrt(size));
+    document.getElementById("grid-container").innerHTML = '';
+    for (let i = 0; i < size; i++) {
+        var gridBox = document.createElement("div");
+        gridBox.className = "grid-box";
+        gridBox.id = "grid-box"+i;
+        gridBox.style.width = boxSize+"px";
+        gridBox.style.height = boxSize+"px";
+        gridBox.addEventListener("mouseover", changeColor);
+        document.getElementById("grid-container").appendChild(gridBox); 
+    };
+    
 }
